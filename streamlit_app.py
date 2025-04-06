95ceb4e3dff89ab93d7b02c5e467320e01e50d60
Jordan Stuart <131272023+jstuart06@users.noreply.github.com>
2025-04-06 14:45:31 -0400

    Update streamlit_app.py
    finished the parathesis on the data frame column

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,9 @@ helpful_links = [
     "https://docs.snowflake.com/en/release-notes/streamlit-in-snowflake"
 ]
 
+# Create a dictionary mapping display values to API values
+# data_mapping = dict(zip(data['display_column'], data['api_column']))
+
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write("Choose the fruits you want in your custom Smoothie!")
@@ -19,7 +22,7 @@ st.write('The name on your Smoothie will be:', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON')
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
